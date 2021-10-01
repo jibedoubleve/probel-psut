c89830f4f62940528c7a26bbf4465dce81a03298
jibedoubleve <jibedoubleve@gmail.com>
2021-10-01 12:46:31 +0200

    (maint) Rewrite move-photo to handle multiple drives

--- src/Photos/Move-Photo.ps1
+++ src/Photos/Move-Photo.ps1
@@ -1,64 +1,82 @@
+
+$path = "$env:USERPROFILE\Documents\cached_date.txt"
+
 function Get-CachedDate {
     $date = Get-Date("1970/1/1")
-
+    
     if (Test-Path $path) {
         $date = Get-Date $(Get-Content $path)
     }
 
-    return $date;
+    return $date
+}
+
+function Set-CachedDate {    
+    Set-Content -Path $path -Value $(Get-Date) -NoNewline -Force
+}
+
+function Copy-File ($file, $dst) {
+
+    $d = $file.LastWriteTime
+    $srcName = '{0:0000}-{1:00}-{2:00}' -f $d.Year, $d.Month, $d.Day
+    $destination = Join-Path -Path $dst -ChildPath $srcName
+
+    if (-not(Test-Path $destination)) {
+        Write-Host " (New DIR)" -ForegroundColor Yellow -NoNewline
+        $c = mkdir $destination
+    }
+    
+    $destPath = Join-Path -Path $destination -ChildPath $file.Name
+    if (-not([System.Io.File]::Exists($destPath))) {
+        Copy-Item -Path $file -Destination $destination
+        Write-Host " DONE " -ForegroundColor DarkGreen
+    }
+    else {
+        Write-Host " SKIPPED" -ForegroundColor DarkRed
+    }
 }
 
 function Move-Photo {
-    param (
-        $drive = "D",
-        $src = ":\DCIM\100CANON",
+    param (        
+        $relativeSrc = "DCIM",
         $dst = "$env:userprofile\Downloads\Photos_Temp",
         $date = $null
     )
     $ErrorActionPreference = "Stop"
-    $path = "$env:USERPROFILE\Documents\cached_date.txt"
-    $src = "$drive$src"
 
     try {
         if ($null -eq $date) {
             $date = Get-CachedDate
-        }
+        }    
 
-        Write-Host "Copying photo from $date to today..."
+        $path = "$env:USERPROFILE\Documents\cached_date.txt"
+        $drives = Get-PSDrive -PSProvider FileSystem
 
-        if ($(Test-Path $src) -eq $false) {
-            throw "The destination directory '$src' do not exist!"
-        }
-        if ($(Test-Path $dst) -eq $false) {
-            mkdir $dst
-        }
+        foreach ($drive in $drives) {
+            Write-Host "Looking into drive $($drive.Root)" -ForegroundColor Cyan
+    
+            $fullPath = Join-Path -Path $drive.Root -ChildPath $relativeSrc
 
-        Push-Location $src
+            if (Test-Path $fullPath) {
+                $dirList = Get-ChildItem -Path $fullPath -Directory | where { $_.Name -Match "\d{3}CANON" }
 
-        $files = Get-ChildItem -Recurse | where LastWriteTime -GT $(Get-Date $date)
-        $dates = (Get-ChildItem -Recurse | where LastWriteTime -GT $(Get-Date $date) | select -ExpandProperty LastWriteTime) | select -ExpandProperty Date -unique
+                foreach ($dir in $dirList) {
+                    Write-Host "`t $($dir.Name)" -ForegroundColor DarkBlue
 
-        foreach ($d in $dates) {    
-            $dir = '{0:0000}-{1:00}-{2:00}' -f $d.Year, $d.Month, $d.Day
-
-            $files | where { ($_.LastWriteTime).Date -eq $d } | % {
-                $dstDir = "$dst\$dir"
-                if ($(Test-Path $dst\$dir) -EQ $false) {
-                    Write-Host "Create directory '$dstDir\'" -ForegroundColor Cyan
-                    $null = mkdir $dstDir
+                    $files = Get-ChildItem -Path $dir -Recurse | where LastWriteTime -GT $(Get-Date $date)                                           
+                    foreach ($file in $files) {  
+                        $dateStr = '{0:0000}-{1:00}-{2:00}' -f $file.LastWriteTime.Year, $file.LastWriteTime.Month, $file.LastWriteTime.Day
+                        Write-Host "`t`tCopying $($file.Name) [$dateStr]..." -NoNewline
+                        Copy-File -file $file -dst $dst
+                    }      
                 } 
-        
-                if (-not(Test-Path "$dstDir\$($_.Name)")) {
-                    Copy-Item -Path $($_.FullName)  -Destination "$dstDir" 
-                }
-                else {
-                    Write-Host "Skip file $($_.Name)." -ForegroundColor Yellow
-                }
             }
         }
-        Set-Content -Path $path -Value $(Get-Date) -NoNewline -Force
+
+        Set-CachedDate
     }
-    finally {
-        Pop-Location
-    }
+    catch {
+        Write-Host "An error occured: $($_.Message)" -ForegroundColor Yellow -BackgroundColor Red
+        Write-Host $_ -ForegroundColor Red
+    }    
 }
