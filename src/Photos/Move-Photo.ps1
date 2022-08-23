c1d95cbf4fcc959b075e946412fb860f504d6ffe
jibedoubleve <jibedoubleve@users.noreply.github.com>
2022-08-23 19:23:44 +0200

    (maint) Add support for MacOs

--- src/Photos/Move-Photo.ps1
+++ src/Photos/Move-Photo.ps1
@@ -1,5 +1,15 @@
+if ($IsMacOS) {
+    $homePath = $env:HOME
+}
+elseif ($IsWindows) {
+    $homePath = $env:USERPROFILE
 
-$path = "$env:USERPROFILE\Documents\cached_date.txt"
+}
+else {
+    throw "Not supported OS"
+}
+
+$path = "$homePath\Documents\cached_date.txt"
 
 function Get-CachedDate {
     $date = Get-Date("1970/1/1")
@@ -22,8 +32,7 @@ function Copy-File ($file, $dst) {
     $destination = Join-Path -Path $dst -ChildPath $srcName
 
     if (-not(Test-Path $destination)) {
-        Write-Host " (New DIR)" -ForegroundColor Yellow -NoNewline
-        $c = mkdir $destination
+        $c = Create-Directory $destination
     }
     
     $destPath = Join-Path -Path $destination -ChildPath $file.Name
@@ -36,10 +45,21 @@ function Copy-File ($file, $dst) {
     }
 }
 
+function Create-Directory ($destination) {
+
+    Write-Host " (New DIR)" -ForegroundColor Yellow -NoNewline
+    if ($IsWindows) {
+        mkdir $destination
+    }
+    elseif ($IsMacOS) {
+        mkdir -p $destination 
+    }
+}
+
 function Move-Photo {
     param (        
         $relativeSrc = "DCIM",
-        $dst = "$env:userprofile\Downloads\Photos_Temp",
+        $dst = "$homePath\Downloads\Photos_Temp",
         $date = $null
     )
     $ErrorActionPreference = "Stop"
@@ -49,28 +69,20 @@ function Move-Photo {
             $date = Get-CachedDate
         }    
 
-        $path = "$env:USERPROFILE\Documents\cached_date.txt"
-        $drives = Get-PSDrive -PSProvider FileSystem
+        if ($IsWindows) {
+            $drives = Get-PSDrive -PSProvider FileSystem
 
-        foreach ($drive in $drives) {
-            Write-Host "Looking into drive $($drive.Root)" -ForegroundColor Cyan
-    
-            $fullPath = Join-Path -Path $drive.Root -ChildPath $relativeSrc
-
-            if (Test-Path $fullPath) {
-                $dirList = Get-ChildItem -Path $fullPath -Directory | where { $_.Name -Match "\d{3}CANON" }
-
-                foreach ($dir in $dirList) {
-                    Write-Host "`t $($dir.Name)" -ForegroundColor DarkBlue
-
-                    $files = Get-ChildItem -Path $dir -Recurse | where LastWriteTime -GT $(Get-Date $date)                                           
-                    foreach ($file in $files) {  
-                        $dateStr = '{0:0000}-{1:00}-{2:00}' -f $file.LastWriteTime.Year, $file.LastWriteTime.Month, $file.LastWriteTime.Day
-                        Write-Host "`t`tCopying $($file.Name) [$dateStr]..." -NoNewline
-                        Copy-File -file $file -dst $dst
-                    }      
-                } 
-            }
+            foreach ($drive in $drives) {
+            
+                $fullPath = Join-Path -Path $drive.Root -ChildPath $relativeSrc
+                Write-Host "(WINDOWS) Looking into drive '$fullPath'" -ForegroundColor Cyan
+                Copy-Files $fullPath
+            } 
+        }
+        elseif ($IsMacOS) {
+            $fullPath = Join-Path -Path "/Volumes/EOS_DIGITAL" -ChildPath $relativeSrc
+            Write-Host "(MacOS) Looking into drive '$fullPath'" -ForegroundColor Cyan
+            Copy-Files $fullPath
         }
 
         Set-CachedDate
@@ -79,4 +91,21 @@ function Move-Photo {
         Write-Host "An error occured: $($_.Message)" -ForegroundColor Yellow -BackgroundColor Red
         Write-Host $_ -ForegroundColor Red
     }    
+}
+
+function Copy-Files($fullPath) {
+    if (Test-Path $fullPath) {
+        $dirList = Get-ChildItem -Path $fullPath -Directory | where { $_.Name -Match "\d{3}CANON" }
+
+        foreach ($dir in $dirList) {
+            Write-Host "`t $($dir.Name)" -ForegroundColor DarkBlue
+
+            $files = Get-ChildItem -Path $dir -Recurse | where LastWriteTime -GT $(Get-Date $date)                                           
+            foreach ($file in $files) {  
+                $dateStr = '{0:0000}-{1:00}-{2:00}' -f $file.LastWriteTime.Year, $file.LastWriteTime.Month, $file.LastWriteTime.Day
+                Write-Host "`t`tCopying $($file.Name) [$dateStr]..." -NoNewline
+                Copy-File -file $file -dst $dst
+            }
+        }      
+    }     
 }
